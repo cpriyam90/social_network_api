02daa9a89ad862413fc84ac347c0e48c95d30cc5
Priyam Carto <cpriyam90@gmail.com>
2022-07-14 19:13:49 -0400

    user route started

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,3 +1,5 @@
+// Lesson 18 - bootcampo spot on Pizza model - https://courses.bootcampspot.com/courses/1196/pages/18-dot-1-7-create-the-pizza-api-routes?module_item_id=462987
+
 const router = require('express').Router();
 const userRoute = require('./user');
 const thoughtRoute = require('./thought');
--- routes/api/user.js
+++ routes/api/user.js
@@ -1,1 +1,22 @@
 // lesson 18 - Pizza Model - https://courses.bootcampspot.com/courses/1196/pages/18-dot-1-6-create-the-pizza-controller?module_item_id=462978
+
+const router = require('express').Router();
+
+const {
+    getallusers,
+    getSingleUserById,
+    createNewUser,
+    updateUserById,
+    eleteuserById
+  } = require('../../controllers/user');
+  
+  // all users
+  router.get('/')
+  router.post('/')
+
+  // user by id
+  router.get('/:id')
+  router.put('/:id')
+  router.delete('/:id')
+  
+  module.exports = router;
