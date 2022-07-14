386244a9aa782c6b68caeb0f47657c9dd4ed5721
Priyam Carto <cpriyam90@gmail.com>
2022-07-14 19:24:17 -0400

    thought routes started

---
+++ routes/api/thought.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const {
+    getallthoughts
+    getSingleThought
+    createThought
+    updateThoughtbyId
+    deleteThought
+} = require('../../controllers/thought');
+
+module.exports = router;
